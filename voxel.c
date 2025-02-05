dfe967c8a7f0411cde579d45b80bfaf821a78262
ranelden <ranelden.co@gmail.com>
2025-02-05 17:11:49 +0100

    supressing changed line( i guess)

--- voxel.c
+++ voxel.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-**int Convert_to_matrice(int*** model, int x_length, int y_length, int z_length, int n){
-    **int voxel_matrice = (int*) malloc(sizeof(int*)); // doit proteger malloc
-    int n = 0;
-    for(int i = 0 ; i < z_length ; i++){
-        for(int j = 0 ; j < y_length ; j++){
-            for(int k = 0 ; k < x_length ; k++){
-                if(model[Z][y][x]==1){
-                    voxel_matrice = realloc(voxel_matrice, sizeof(int*)*(n+1))  // doit proteger malloc
-                    voxel_matrice[n][0] =  x_length;
-                    voxel_matrice[n][2] =  z_length;
-                    n++;
 int** Convert_to_matrice(int*** model, int x_length, int y_length, int z_length, int* n) {
     int** voxel_matrice = NULL; // Tableau 2D stockant des (x, y, z)
     *n = 0; // Nombre de voxels actifs trouvés
@@ -47,7 +36,6 @@ int** Convert_to_matrice(int*** model, int x_length, int y_length, int z_length,
             }
         }
     }
-}
 
     return voxel_matrice;
 }
