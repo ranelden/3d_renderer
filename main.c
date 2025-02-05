106d6cb9383e5b98893b0ab54221e786ad68cb99
ranelden <ranelden.co@gmail.com>
2025-02-05 17:05:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <matrice.c>
 
+#define objet  {{{1,1,1},
+                 {1,1,1},
+                 {1,1,1}},
+                
+                {{0,1,0},
+                 {1,1,1},
+                 {0,1,0}},
 
+                {{0,0,0},
+                 {0,1,0},
+                 {0,0,0}}}
 
 int main(){
     
 
+    
 
     return 0;
 }
