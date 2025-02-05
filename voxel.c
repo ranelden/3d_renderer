41052bca6e85bf298c0a4ab2dd76ea96acdcea38
ranelden <ranelden.co@gmail.com>
2025-02-05 17:05:23 +0100

    Update voxel.c

--- voxel.c
+++ voxel.c
@@ -13,8 +13,42 @@
                     voxel_matrice[n][1] =  y_length;
                     voxel_matrice[n][2] =  z_length;
                     n++;
+int** Convert_to_matrice(int*** model, int x_length, int y_length, int z_length, int* n) {
+    int** voxel_matrice = NULL; // Tableau 2D stockant des (x, y, z)
+    *n = 0; // Nombre de voxels actifs trouvés
+
+    for (int i = 0; i < z_length; i++) {
+        for (int j = 0; j < y_length; j++) {
+            for (int k = 0; k < x_length; k++) {
+                if (model[i][j][k] == 1) {  // Si le voxel est actif
+
+                    // Réallocation pour un nouvel élément
+                    int** temp = realloc(voxel_matrice, (*n + 1) * sizeof(int*));
+                    if (!temp) {
+                        free(voxel_matrice); // Libération mémoire en cas d'échec
+                        return NULL;
+                    }
+                    voxel_matrice = temp;
+
+                    // Allocation d'une ligne pour (x, y, z)
+                    voxel_matrice[*n] = malloc(3 * sizeof(int));
+                    if (!voxel_matrice[*n]) {
+                        for (int l = 0; l < *n; l++) free(voxel_matrice[l]);
+                        free(voxel_matrice);
+                        return NULL;
+                    }
+
+                    // Stockage des coordonnées
+                    voxel_matrice[*n][0] = k;
+                    voxel_matrice[*n][1] = j;
+                    voxel_matrice[*n][2] = i;
+
+                    (*n)++;  // Incrémentation du compteur
                 }
             }
         }
     }
 }
+    return voxel_matrice;
+}
+
